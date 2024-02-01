9ce229e3049b5064e3d1551d122b6f0e784611ce
donsolo-khalifa <101540488+donsolo-khalifa@users.noreply.github.com>
2024-02-02 00:01:53 +0100

    blog page setup

--- larablog/app/Http/Controllers/PostController.php
+++ larablog/app/Http/Controllers/PostController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    function index()
+    {
+        return view(
+            'posts.index',
+            ['posts' => Post::take(5)->get()]
+        );
+    }
 }
--- larablog/app/Models/Post.php
+++ larablog/app/Models/Post.php
@@ -5,10 +5,20 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
     function scopePublished($query)
     {
         $query->where('published_at', '<=', Carbon::now());
@@ -16,5 +26,16 @@ class Post extends Model
     function scopeFeatured($query)
     {
         $query->where('featured', true);
+    }
+
+    function getExcerpt()
+    {
+        return Str::limit(strip_tags($this->content), 150);
+    }
+
+    function getReadingTime()
+    {
+        $mins = round(str_word_count($this->content) / 250);
+        return ($mins < 1) ? 1  : $mins;
     }
 }
